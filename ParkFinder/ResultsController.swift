dc53f1454460c586bf3c1fafb76b63ebcff77d6a
Andre Meneghetti <meneghetti1512@gmail.com>
2018-11-03 11:58:03 -0400

    1

--- ParkFinder/ResultsController.swift
+++ ParkFinder/ResultsController.swift
@@ -14,7 +14,7 @@ class ResultsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
