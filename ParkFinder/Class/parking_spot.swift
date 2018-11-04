081a68590934266118a04d3a30814dc9a0f5c005
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2018-11-03 23:43:12 -0400

    Implement mock data

--- ParkFinder/Class/parking_spot.swift
+++ ParkFinder/Class/parking_spot.swift
@@ -9,16 +9,70 @@
 import UIKit
 
 class parking_spot: NSObject {
-    let type           : Int // Parking Lot = 1, Personal = 2
+    var type           : Int // Parking Lot = 1, Personal = 2
+    var name           : String?
     var min_price_hour : Double
     var address        : String
-    var availability   : [[Int]] // Nx7 matrix with availability for each day
-    var ratings        : [Int]?
+    var rating         : Double
+    var availability   : [[(start: Int, end: Int)]] // Nx7 matrix with availability for each day
     
-    init(type: Int, availability: [[Int]], min_price_hour: Double, address: String) {
+    init(type: Int, name: String?, min_price_hour: Double, address: String, rating: Double, availability: [[(start: Int, end: Int)]]) {
         self.type           = type
-        self.availability   = availability
+        self.name           = name
         self.min_price_hour = min_price_hour
         self.address        = address
+        self.rating         = rating
+        self.availability   = availability
     }
+    
+    static let MOCK_DATA: [parking_spot] = [
+        parking_spot(type: 1,
+                     name: "Quik Park Theater Lot LLC",
+                     min_price_hour: 15.0,
+                     address: "223 W 46th St, New York, NY",
+                     rating: 3.3,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 1,
+                     name: "80 Car Park",
+                     min_price_hour: 10.0,
+                     address: "80 Park Ave, New York, NY",
+                     rating: 4.1,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 1,
+                     name: "Impark",
+                     min_price_hour: 13.0,
+                     address: "305 W 42nd St, New York, NY",
+                     rating: 4.3,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 1,
+                     name: "Icon Parking Systems",
+                     min_price_hour: 13.5,
+                     address: "15 W 70th St, New York, NY",
+                     rating: 4.5,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 1,
+                     name: "LAZ Parking Lot",
+                     min_price_hour: 12.0,
+                     address: "300 W 31st St, New York, NY",
+                     rating: 3.3,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 2,
+                     name: nil,
+                     min_price_hour: 7.0,
+                     address: "315 W 55th St, New York, NY",
+                     rating: 4.0,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 2,
+                     name: nil,
+                     min_price_hour: 7.5,
+                     address: "321 W 54th St, New York, NY",
+                     rating: 3.2,
+                     availability: [[(1, 1)]]),
+        parking_spot(type: 2,
+                     name: nil,
+                     min_price_hour: 6.0,
+                     address: "24 W 55th St, New York, NY",
+                     rating: 4.5,
+                     availability: [[(1, 1)]])
+        ]
 }
